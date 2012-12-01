be1e02e532cd8a716b9266afb28e20d36ffe31eb
RenanSoranso <RenanSoranso@022cea56-e8e0-7de4-cdce-0cb1385dd2bc>
2012-12-01 21:16:34 +0000

    Area para Curriculo adicionada

--- Invio/src/java/invio/bean/CurriculoBean.java
+++ Invio/src/java/invio/bean/CurriculoBean.java
@@ -1,23 +1,84 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package invio.bean;
 
+import invio.entidade.Curriculo;
+import invio.rn.CurriculoRN;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-/**
- *
- * @author Junior
- */
 @ManagedBean
 @RequestScoped
 public class CurriculoBean {
 
-    /**
-     * Creates a new instance of CurriculoBean
-     */
+    private CurriculoRN curriculoRN = new CurriculoRN();
+    private List<Curriculo> curriculos;
+    private Curriculo curriculo;
+
+    public CurriculoBean(List<Curriculo> curriculos) {
+        this.curriculos = curriculos;
+    }
+
     public CurriculoBean() {
+    }
+
+    public List<Curriculo> getCurriculos() {
+        //    if (instituicoes == null) {
+        curriculos = curriculoRN.obterTodos();
+        //  }
+        return curriculos;
+    }
+
+    public void setCurriculos(List<Curriculo> curriculos) {
+        this.curriculos = curriculos;
+    }
+
+    public Curriculo getCurriculo() {
+        if (curriculo != null) {
+            return curriculo;
+        } else {
+            return (Curriculo) BeanUtil.lerDaSessao("curriculo");
+        }
+    }
+
+    public void setCurriculo(Curriculo curriculo) {
+        if (BeanUtil.lerDaSessao("curriculo") == null) {
+            BeanUtil.colocarNaSessao("curriculo", curriculo);
+        }
+        this.curriculo = curriculo;
+    }
+
+    public String salvarCurriculo() {
+        if (curriculoRN.salvar(curriculo)) {
+            BeanUtil.criarMensagemDeInformacao(
+                    "Operação realizada com sucesso",
+                    "O curriculo " + curriculo.getNome() + " foi gravado com sucesso.");
+
+        } else {
+            BeanUtil.criarMensagemDeErro("Erro ao salvar o curriculo", "Curriculo: " + curriculo.getNome());
+        }
+
+        return "/cadastro/curriculo/listar.xhtml";
+    }
+
+    public String excluirCurriculo() {
+        System.out.println("Curriculo " + curriculo);
+        if (curriculoRN.remover(curriculo)) {
+            BeanUtil.criarMensagemDeInformacao("Curriculo excluído", "Curriculo: " + curriculo.getNome());
+        } else {
+            BeanUtil.criarMensagemDeErro("Erro ao excluir o curriculo", "Curriculo: " + curriculo.getNome());
+        }
+        return "listar.xhtml";
+    }
+
+    public String irListarCurriculos() {
+        curriculo = null;
+
+        return "/cadastro/curriculo/listar.xhtml";
+    }
+
+    public String novoFormularioCurriculo() {
+
+        curriculo = new Curriculo();
+        return "/cadastro/curriculo/formulario.xhtml";
     }
 }
