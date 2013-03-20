9a4dd18696cca32e3fcd1e586c9d6d6377cd1151
enemiasjunior <enemiasjunior@022cea56-e8e0-7de4-cdce-0cb1385dd2bc>
2013-03-20 03:37:14 +0000

    Qualis com salvar inciando e terminando transação

--- Invio/src/java/invio/rn/QualisRN.java
+++ Invio/src/java/invio/rn/QualisRN.java
@@ -24,6 +24,43 @@ public class QualisRN {
         }
     }
 
+    public int salvar(List<Qualis> osQualis) {
+        boolean confirmar = true;
+        int registros = 0;
+        int i = 0;
+        final int PARAR = 100;
+        if (dao.iniciarTransacao()) {
+            for (Qualis qualis : osQualis) {
+                if (qualis.getId() == null) {
+                    confirmar = dao.criar(qualis);
+                } else {
+                    confirmar = dao.alterar(qualis);
+                }
+                if (!confirmar) {
+                    break;
+                } else {
+                    i++;
+                    if (i > PARAR) {
+                        if (!dao.concluirTransacao()) {
+                            return registros;
+                        } else {
+                            System.out.println(registros + "Salvos!");
+                            registros += i;
+                        }
+                        if (!dao.iniciarTransacao()) {
+                            return registros;
+                        }
+                        i = 0;
+                    }
+
+                }
+            }
+            return registros;
+        } else {
+            return registros;
+        }
+    }
+
     public boolean remover(Qualis qualis) {
         return dao.excluir(qualis);
     }
