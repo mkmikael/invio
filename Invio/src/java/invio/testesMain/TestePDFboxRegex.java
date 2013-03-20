117ebbc65820ca3e22f032295c4a147597514f6c
enemiasjunior <enemiasjunior@022cea56-e8e0-7de4-cdce-0cb1385dd2bc>
2013-03-20 03:38:07 +0000

    Salvando lista de qualis, inciando e terminando transação

--- Invio/src/java/invio/testesMain/TestePDFboxRegex.java
+++ Invio/src/java/invio/testesMain/TestePDFboxRegex.java
@@ -8,6 +8,7 @@ import invio.entidade.Qualis;
 import invio.rn.PDFBoxRN;
 import invio.rn.PDFRegexRN;
 import invio.rn.QualisRN;
+import java.util.ArrayList;
 import javax.swing.JFileChooser;
 
 /**
@@ -15,34 +16,29 @@ import javax.swing.JFileChooser;
  * @author Junior
  */
 public class TestePDFboxRegex {
-    
+
     public static void main(String[] args) {
-        
-        
+
+
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.showOpenDialog(null);
-        
+
         PDFBoxRN pdfBoxRn = new PDFBoxRN();
         PDFRegexRN pdfRegexRn = new PDFRegexRN();
-        
+
         QualisRN qualisRN = new QualisRN();
         Qualis qualis = new Qualis();
-        
+
         String[] splitConteudoPDF = pdfBoxRn.getConteudoPDF(fileChooser.getSelectedFile().getPath()).split("\n");
-        
+        ArrayList<Qualis> osQualis = new ArrayList<Qualis>();
         for (int i = 0; i < splitConteudoPDF.length; i++) {
-            
             if (pdfRegexRn.procurarPadroesNaLinha(qualis, splitConteudoPDF[i])) {
-                qualisRN.salvar(qualis);
+                osQualis.add(qualis);
                 qualis = new Qualis();
-            }else{
+            } else {
                 continue;
             }
-            
         }
-        
-        
-        
+        System.out.println(qualisRN.salvar(osQualis));
     }
-    
 }
