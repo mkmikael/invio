4860cd65bbf611be49e23e4d89ebede85fb8f29c
RenanSoranso <RenanSoranso@022cea56-e8e0-7de4-cdce-0cb1385dd2bc>
2013-04-24 20:39:45 +0000

    Teste

--- Invio/src/java/invio/bean/PlanoBean.java
+++ Invio/src/java/invio/bean/PlanoBean.java
@@ -1,9 +1,52 @@
 package invio.bean;
 
+import invio.entidade.Plano;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Renan
  */
 public class PlanoBean {
+    
+    private Plano plano = new Plano();
+    private List<Plano> planos;
+
+    public Plano getPlano() {
+        return plano;
+    }
+
+    public void setPlano(Plano plano) {
+        this.plano = plano;
+    }
+
+    public List<Plano> getPlanos() {
+        return planos;
+    }
+
+    public void setPlanos(List<Plano> planos) {
+        this.planos = planos;
+    }
+    
+    
+    
+    
+    public void salvar(){
+        
+    }
+    
+    public void excluir(){
+        
+    }
+    
+    public List<Plano> listar(){
+        return planos;
+    }
+    
+    public void salvarCurriculo(){
+        
+    }
+    
     
 }
