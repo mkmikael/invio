5c134a7f6550f6f97020edfcd3af6958ae89a4bc
marcosdjufra <marcosdjufra@022cea56-e8e0-7de4-cdce-0cb1385dd2bc>
2013-04-25 20:14:27 +0000

    Atualização da classe PlanoRN

---
+++ Invio/src/java/invio/dao/QualisDAO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package invio.dao;
+
+import invio.entidade.Qualis;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Dir de Armas Marinha
+ */
+public class QualisDAO extends GenericDAO<Qualis> {
+
+    public String obterEstrato(String titulo, String area) {
+
+
+        String query = "SELECT q.estrado FROM qualis WHERE q.titulo = titulo AND q.areaAvaliacao = area";
+        Query q = getEntityManager().createQuery(query);
+
+        q.setParameter("titulo", titulo);
+        q.setParameter("area", area);
+
+        String resultado = (String) q.getSingleResult();
+
+        return resultado;
+
+    }
+}
