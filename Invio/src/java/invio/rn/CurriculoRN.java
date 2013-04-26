10d23649cebc045366a03fbea043d1f5be5979cd
RenanSoranso <RenanSoranso@022cea56-e8e0-7de4-cdce-0cb1385dd2bc>
2013-04-26 20:41:36 +0000

    metodo obterTodosDesc no RN

--- Invio/src/java/invio/rn/CurriculoRN.java
+++ Invio/src/java/invio/rn/CurriculoRN.java
@@ -6,7 +6,15 @@ package invio.rn;
 
 import invio.dao.GenericDAO;
 import invio.entidade.Curriculo;
+import invio.entidade.Livro;
+import invio.entidade.Periodico;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -45,4 +53,79 @@ public class CurriculoRN {
     public List<Curriculo> obterTodos() {
         return dao.obterTodos(Curriculo.class);
     }
+
+   public List<Curriculo> obterTodosDesc() {
+        List<Curriculo> obterTodosDesc = new ArrayList<Curriculo>();
+
+        List<CurriculoPts> mapa = new ArrayList<CurriculoPts>();
+
+        for (Curriculo curriculo : dao.obterTodos(Curriculo.class)) {
+
+            int totalP = 0;
+            int totalL = 0;
+
+            for (Periodico periodico : curriculo.getPeriodicoList()) {
+                totalP += periodico.getEstrato();
+            }
+
+            for (Livro livro : curriculo.getLivroList()) {
+                totalL += livro.getEstrato();
+            }
+            CurriculoPts cpts = new CurriculoPts(curriculo, totalP+totalL);
+            mapa.add(cpts);
+
+        }
+        
+        Collections.sort(mapa);
+        
+        
+        for (CurriculoPts curriculoPts : mapa) {
+            obterTodosDesc.add(curriculoPts.getC());
+        }
+        
+        
+        return obterTodosDesc;
+    }
+    
+    
+    
+}
+
+
+class CurriculoPts implements Comparable<CurriculoPts> {
+    private Curriculo c;
+    private Integer pts;
+
+    public CurriculoPts(Curriculo c, Integer pts) {
+        this.c = c;
+        this.pts = pts;
+    }
+    public Curriculo getC() {
+        return c;
+    }
+
+    public void setC(Curriculo c) {
+        this.c = c;
+    }
+
+    public Integer getPts() {
+        return pts;
+    }
+
+    public void setPts(Integer pts) {
+        this.pts = pts;
+    }
+
+    @Override
+    public int compareTo(CurriculoPts o) {
+        if (this.pts < o.getPts()) {
+            return -1;
+        }
+        if (this.pts > o.getPts()) {
+            return 1;
+        }
+        return 0;
+    }
+
+  
 }
