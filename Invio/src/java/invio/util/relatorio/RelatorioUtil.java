f3dfa4a07da543b291baac3badbdcceb0bb1f410
flbezerra@gmail.com <flbezerra@gmail.com@022cea56-e8e0-7de4-cdce-0cb1385dd2bc>
2013-05-20 00:33:22 +0000

    iniciando implementação do esquema de relatórios!

--- Invio/src/java/invio/util/relatorio/RelatorioUtil.java
+++ Invio/src/java/invio/util/relatorio/RelatorioUtil.java
@@ -4,10 +4,53 @@
  */
 package invio.util.relatorio;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
+
 /**
  *
  * @author fabio
  */
-public class RelatorioUtil {
-    
+public class RelatorioUtil<T> {
+
+    public boolean criarPDF(
+            Connection conexao,
+            File arquivoJasper,
+            File pdf) {
+        try {
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(arquivoJasper);
+            JasperPrint fillReport = JasperFillManager.fillReport(jasperReport, null, conexao);
+            JasperExportManager.exportReportToPdfFile(fillReport, pdf.getAbsolutePath());
+            conexao.close();
+            return true;
+        } catch (JRException ex) {
+            return false;
+        } catch (SQLException ex) {
+            return false;
+        }
+    }
+
+    public boolean criarPDF(
+            List<T> objetos,
+            File arquivoJasper,
+            File pdf) {
+        try {
+            JRDataSource dataSource = new JRBeanCollectionDataSource(objetos);
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(arquivoJasper);
+            JasperPrint fillReport = JasperFillManager.fillReport(jasperReport, null, dataSource);
+            return true;
+        } catch (JRException ex) {
+            return false;
+        }
+    }
 }
