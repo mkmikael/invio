33cc0375eb52368757decb5c5c06cfdc7420883b
fabricio.brabo <fabricio.brabo@022cea56-e8e0-7de4-cdce-0cb1385dd2bc>
2014-05-07 03:26:19 +0000

    tratamento de erro no caso de geração de FCO, para quando o FCO é nulo e implementação quase total dos relatórios de FCO commit 2

--- Invio/src/java/invio/util/relatorio/Relatorio.java
+++ Invio/src/java/invio/util/relatorio/Relatorio.java
@@ -1,5 +1,6 @@
 package invio.util.relatorio;
 
+import invio.dao.ConexaoDB;
 import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.List;
@@ -8,6 +9,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
@@ -17,19 +19,26 @@ import net.sf.jasperreports.engine.export.JRPdfExporter;
  *
  * @author fabio
  */
-public class Relatorio<T> {
+public class Relatorio {
 
-       public static void geraRelatorio(String path, List dataSource, String name) {
+    public static void geraRelatorio(String path, 
+            String name, 
+            Object params, 
+            Object Area) {
 
-        JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(dataSource);
         HashMap parameters = new HashMap();
+        parameters.put("Meu_Parametro", params);
+        parameters.put("Area", Area);
 
         try {
             FacesContext facesContext = FacesContext.getCurrentInstance();
             facesContext.responseComplete();
             ServletContext scontext = (ServletContext) facesContext.getExternalContext().getContext();
-            
-            JasperPrint jasperPrint = JasperFillManager.fillReport(scontext.getRealPath(path), parameters, ds);
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(
+                    scontext.getRealPath(path), 
+                    parameters, 
+                    ConexaoDB.conectar());
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             JRPdfExporter exporter = new JRPdfExporter();
             exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
@@ -39,7 +48,8 @@ public class Relatorio<T> {
             byte[] bytes = baos.toByteArray();
 
             if (bytes != null && bytes.length > 0) {
-                HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+                HttpServletResponse response =
+                        (HttpServletResponse) facesContext.getExternalContext().getResponse();
                 response.setContentType("application/pdf");
                 response.setHeader("Content-disposition", "inline; filename=\"" + name + ".pdf\"");
                 response.setContentLength(bytes.length);
@@ -50,6 +60,37 @@ public class Relatorio<T> {
             }
         } catch (Exception e) {
             System.out.println("Exception: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public static void ImprimirRelatorio(String path, List dataSource, String name, Object params, Object Area) {
+        try {
+
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            ServletContext scontext =
+                    (ServletContext) facesContext.getExternalContext().getContext();
+
+            JRBeanCollectionDataSource collection = new JRBeanCollectionDataSource(dataSource);
+            HashMap<String, Object> parameters = new HashMap<String, Object>();
+            parameters.put("Meu_Parametro", params);
+            parameters.put("Area", Area);
+            JasperPrint print = JasperFillManager.fillReport(scontext.getRealPath(path),
+                    parameters, collection);
+
+            byte[] b = JasperExportManager.exportReportToPdf(print);
+            JRPdfExporter exporter = new JRPdfExporter();
+            exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+            exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, b);
+            exporter.exportReport();
+
+            HttpServletResponse res = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+            res.setContentType("application/pdf");
+            res.setHeader("Content-disposition", "inline; filename=\"" + name + ".pdf\"");
+            res.getOutputStream().write(b);
+            facesContext.renderResponse();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
