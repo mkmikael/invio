5ea7dc1cc0aabf575b7ae4089d05e922c1090016
silvarney@gmail.com <silvarney@gmail.com@022cea56-e8e0-7de4-cdce-0cb1385dd2bc>
2015-03-11 21:08:29 +0000

    Modificações, Silvarney. 11 de março

--- Invio2/src/main/webapp/resources/js/locales.js
+++ Invio2/src/main/webapp/resources/js/locales.js
@@ -2,6 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 PrimeFaces.locales['pt_BR'] = {
     closeText : 'Fechar',
     prevText : 'Anterior',
