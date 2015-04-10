51e4023a9c08a617aa13abfdf6bb9c2bea6e9ddf
mkmikael07 <mkmikael07@022cea56-e8e0-7de4-cdce-0cb1385dd2bc>
2015-04-10 23:46:37 +0000

    Versao MIKAEL

--- InvioScript/src/main/java/bpmlab/invioscript/ConstruirQualis.java
+++ InvioScript/src/main/java/bpmlab/invioscript/ConstruirQualis.java
@@ -10,8 +10,6 @@ import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.JFrame;
-import javax.swing.JProgressBar;
 
 /**
  *
@@ -19,190 +17,137 @@ import javax.swing.JProgressBar;
  */
 public class ConstruirQualis {
 
-    private boolean comBarraDeProgressao = false;
-    private boolean comLogDeMontagem = false;
-    private int total = 0;
-    private int invalidos = 0;
-    private int erros = 0;
-    private final List<Qualis> lista = new ArrayList<>();
-
-    public ConstruirQualis() {
-    }
-
-    public ConstruirQualis(boolean comBarraDeProgressao, boolean comLogDeMontagem) {
-        this.comBarraDeProgressao = comBarraDeProgressao;
-        this.comLogDeMontagem = comLogDeMontagem;
-    }
-
-    public List<Qualis> contruir() throws IOException {
-        lista.clear();
-        PdfReader pdfReader = new PdfReader("/home/bpmlab/NetBeansProjects/InvioScript/src/main/java/bpmlab/invioscript/Consulta_Webqualis.pdf");
-        String[] page;
-        JFrame frame = new JFrame("Qualis");
-        frame.setSize(600, 100);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        if (comBarraDeProgressao) {
-            frame.setVisible(true);
-        }
-        JProgressBar progressBar = new JProgressBar(0, 114937);
-        frame.getContentPane().add(progressBar);
-
-        for (int i = 1; i <= pdfReader.getNumberOfPages(); i++) {
-            page = PdfTextExtractor.getTextFromPage(pdfReader, i).split("\n");
-            for (int j = 1; j < page.length; j++) {
-                try {
-                    if (page[j].contains("Friday 06 March 2015")) {
-                        invalidos++;
-                        continue;
-                    }
-
-                    if (isMaiusculo(page[j])) {
-                        StringBuilder builder = new StringBuilder(page[j + 1]);
-                        int index = page[j + 1].indexOf("Atualizado") - 1;
-                        builder.insert(index, " " + page[j] + " " + page[j + 2]);
-                        montagem(builder.toString());
-                        j += 2;
-                    } else {
-                        montagem(page[j]);
-                    }
-                } catch (Exception ex) {
-                    erros++;
-                } catch (AssertionError ex) {
-                    invalidos++;
-                } finally {
+    public static List<String> primeiraValidacao() {
+        try {
+            PdfReader pdfReader = new PdfReader("/home/bpmlab/NetBeansProjects/InvioScript/src/main/java/bpmlab/invioscript/Consulta_Webqualis.pdf");
+            String[] linha;
+            String novaLinha = null;
+            List<String> qualis = new ArrayList<>();
+            int total = 0;
+            int invalidos = 0;
+            for (int i = 1; i <= pdfReader.getNumberOfPages(); i++) {
+                linha = PdfTextExtractor.getTextFromPage(pdfReader, i).split("\n");
+                for (int j = 1; j < linha.length; j++) {
                     total++;
-                    progressBar.setValue(total);
-                    progressBar.repaint();
+                    try {
+                        if (linha[j].contains("Friday 06 March 2015")
+                                || linha[j].contains("TURISMO")
+                                || linha[j].contains("INTERNACIONAIS")
+                                || linha[j].contains("DEMOGRAFIA")
+                                || linha[j].contains("Lado C")
+                                || linha[j].contains("y TA Journal of Food C")
+                                || linha[j].contains("www.siicsalud.com C NUTRIÇÃO Atualizado")
+                                || linha[j].contains("ISSN TÍTULO ESTRATO ÁREA DE AVALIAÇÃO STATUS")) {
+                            throw new Exception();
+                        }
+
+                        if (!linha[j].contains("Atualizado")) {
+                            throw new Exception();
+                        }
+
+                        int indexFinal = linha[j].indexOf("Atualizado");
+
+                        if (linha[j].contains(" A1 ")) {
+                            novaLinha = linha[j].substring(linha[j].indexOf(" A1 ") + 4, indexFinal);
+                        } else if (linha[j].contains(" A2 ")) {
+                            novaLinha = linha[j].substring(linha[j].indexOf(" A2 ") + 4, indexFinal);
+                        } else if (linha[j].contains(" B1 ")) {
+                            novaLinha = linha[j].substring(linha[j].indexOf(" B1 ") + 4, indexFinal);
+                        } else if (linha[j].contains(" B2 ")) {
+                            novaLinha = linha[j].substring(linha[j].indexOf(" B2 ") + 4, indexFinal);
+                        } else if (linha[j].contains(" B3 ")) {
+                            novaLinha = linha[j].substring(linha[j].indexOf(" B3 ") + 4, indexFinal);
+                        } else if (linha[j].contains(" B4 ")) {
+                            novaLinha = linha[j].substring(linha[j].indexOf(" B4 ") + 4, indexFinal);
+                        } else if (linha[j].contains(" B5 ")) {
+                            novaLinha = linha[j].substring(linha[j].indexOf(" B5 ") + 4, indexFinal);
+                        } else if (linha[j].contains(" C ")) {
+                            novaLinha = linha[j].substring(linha[j].indexOf(" C ") + 3, indexFinal);
+                        } else {
+                            throw new Exception();
+                        }
+
+                        if (!linha[j].substring(0, 9).matches("\\w\\w\\w\\w-\\w\\w\\w\\w")
+                                || linha[j].substring(0, 12).matches("\\w\\w\\w\\w-\\w\\w\\w\\w A1")
+                                || linha[j].substring(0, 12).matches("\\w\\w\\w\\w-\\w\\w\\w\\w A2")
+                                || linha[j].substring(0, 12).matches("\\w\\w\\w\\w-\\w\\w\\w\\w B1")
+                                || linha[j].substring(0, 12).matches("\\w\\w\\w\\w-\\w\\w\\w\\w B2")
+                                || linha[j].substring(0, 12).matches("\\w\\w\\w\\w-\\w\\w\\w\\w B3")
+                                || linha[j].substring(0, 12).matches("\\w\\w\\w\\w-\\w\\w\\w\\w B4")
+                                || linha[j].substring(0, 12).matches("\\w\\w\\w\\w-\\w\\w\\w\\w B5")
+                                || linha[j].substring(0, 12).matches("\\w\\w\\w\\w-\\w\\w\\w\\w C ")) {
+                            throw new Exception();
+                        }
+                        if (novaLinha != null) {
+                            qualis.add(linha[j]);
+                        }
+                        novaLinha = null;
+                    } catch (Exception e) {
+                        StringBuilder construirLinha;
+                        switch (linha[j]) {
+                            case "ADMINISTRAÇÃO, CIÊNCIAS CONTÁBEIS E":
+                                construirLinha = new StringBuilder(linha[j + 1]);
+                                construirLinha.insert(linha[j + 1].indexOf("Atualizado") - 1,
+                                        " " + linha[j] + " " + linha[j + 2]);
+                                qualis.add(construirLinha.toString());
+                                break;
+                            case "CIÊNCIA POLÍTICA E RELAÇÕES":
+                                construirLinha = new StringBuilder(linha[j + 1]);
+                                construirLinha.insert(linha[j + 1].indexOf("Atualizado") - 1,
+                                        " " + linha[j] + " " + linha[j + 2]);
+                                qualis.add(construirLinha.toString());
+                                break;
+                            case "PLANEJAMENTO URBANO E REGIONAL /":
+                                construirLinha = new StringBuilder(linha[j + 1]);
+                                construirLinha.insert(linha[j + 1].indexOf("Atualizado") - 1,
+                                        " " + linha[j] + " " + linha[j + 2]);
+                                qualis.add(construirLinha.toString());
+                                break;
+                            case "American Journal of Physiology. Regulatory, Integrative and Comparative Physiology":
+                                construirLinha = new StringBuilder(linha[j + 1]);
+                                construirLinha.insert(9, " " + linha[j]);
+                                qualis.add(construirLinha.toString());
+                                break;
+                            case "Proceedings of the National Academy of Sciences of the United States of America":
+                                construirLinha = new StringBuilder(linha[j + 1]);
+                                construirLinha.insert(9, " " + linha[j] + linha[j + 2]);
+                                qualis.add(construirLinha.toString());
+                                break;
+                            case "Revista de Clínica e Pesquisa Odontológica (Impresso) / Journal of Dental Clinical and":
+                                construirLinha = new StringBuilder(linha[j + 1]);
+                                construirLinha.insert(9, " " + linha[j] + " " + linha[j + 2]);
+                                qualis.add(construirLinha.toString());
+                                break;
+                            default:
+                                invalidos++;
+                                if (!(linha[j].contains("Friday 06 March")
+                                        || linha[j].contains("TURISMO")
+                                        || linha[j].contains("(Online)")
+                                        || linha[j].contains("Research")
+                                        || linha[j].contains("INTERNACIONAIS")
+                                        || linha[j].contains("DEMOGRAFIA"))) {
+//                                    System.out.println(linha[j]);
+                                }
+                                break;
+                        }
+                    }
                 }
             }
-        }
-        pdfReader.close();
-        return lista;
-    }
-
-    public void logEstatistica() {
-        System.out.println("Total: " + total);
-        System.out.println("Validos:" + (lista.size()) + ";" + ((float) lista.size() / (float) total) * 100 + "%");
-        System.out.println("Invalidos: " + invalidos + ";" + ((float) invalidos / (float) total) * 100 + "%");
-        System.out.println("Erros: " + erros + ";" + ((float) erros / (float) total) * 100 + "%");
-    }
-
-    private boolean isMaiusculo(String string) {
-        char[] letras = string.toCharArray();
-        for (int i = 0; i < string.length(); i++) {
-            if (letras[i] == ',' || letras[i] == ' ') {
-                continue;
+            for (String q : qualis) {
+                System.out.println(q);
             }
-            if (Character.isLowerCase(letras[i])) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private void montagem(String linha) {
-        String issn = null;
-        if (linha.substring(0, 9).matches("\\w\\w\\w\\w-\\w\\w\\w\\w")) {
-            issn = linha.substring(0, 9);
-        } else {
-            throw new AssertionError();
-        }
-
-        String titulo = null;
-        String estrato = null;
-        if (linha.contains(" A1 ")) {
-            estrato = "A1";
-            titulo = linha.substring(10, linha.indexOf("A1"));
-        } else if (linha.contains(" A2 ")) {
-            estrato = "A2";
-            titulo = linha.substring(10, linha.indexOf("A2"));
-        } else if (linha.contains(" B1 ")) {
-            estrato = "B1";
-            titulo = linha.substring(10, linha.indexOf("B1"));
-        } else if (linha.contains(" B2 ")) {
-            estrato = "B2";
-            titulo = linha.substring(10, linha.indexOf("B2"));
-        } else if (linha.contains(" B3 ")) {
-            estrato = "B3";
-            titulo = linha.substring(10, linha.indexOf("B3"));
-        } else if (linha.contains(" B4 ")) {
-            estrato = "B4";
-            titulo = linha.substring(10, linha.indexOf("B4"));
-        } else if (linha.contains(" B5 ")) {
-            estrato = "B5";
-            titulo = linha.substring(10, linha.indexOf("B5"));
-        } else if (linha.contains(" C ")) {
-            estrato = "C";
-            titulo = linha.substring(10, linha.indexOf("C "));
-        } else {
-            throw new AssertionError();
-        }
-        int inicioAreaDeAvaliacao = linha.indexOf(estrato) + 3;
-        int fimAreaDeAvaliacao = linha.indexOf("Atualizado");
-        String areaDeAvaliacao = linha.substring(inicioAreaDeAvaliacao - 1, fimAreaDeAvaliacao);
-
-        if (!linha.contains(" Atualizado")) {
-            throw new AssertionError();
-        }
-
-        if (titulo.trim().isEmpty()) {
-            throw new AssertionError();
-        }
-
-        Qualis qualis = new Qualis(issn.trim(), titulo.trim(), estrato.trim(), areaDeAvaliacao.trim(), "Atualizado");
-        lista.add(qualis);
-        if (comLogDeMontagem) {
-            System.out.println(qualis);
+            System.out.println("TOTAL: " + total);
+            System.out.println("VALIDOS: " + qualis.size() + ";" + ((float) qualis.size() * 100 / total) + "%");
+            System.out.println("INVALIDOS: " + invalidos + ";" + ((float) invalidos * 100 / total) + "%");
+            System.out.println(qualis.size() + invalidos);
+            return qualis;
+        } catch (IOException ex) {
+            return null;
         }
     }
 
-    public boolean isComBarraDeProgressao() {
-        return comBarraDeProgressao;
-    }
-
-    public void setComBarraDeProgressao(boolean comBarraDeProgressao) {
-        this.comBarraDeProgressao = comBarraDeProgressao;
-    }
-
-    public boolean isComLogDeMontagem() {
-        return comLogDeMontagem;
-    }
-
-    public void setComLogDeMontagem(boolean comLogDeMontagem) {
-        this.comLogDeMontagem = comLogDeMontagem;
-    }
-
-    public int getTotal() {
-        return total;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
-
-    public int getInvalidos() {
-        return invalidos;
-    }
-
-    public void setInvalidos(int invalidos) {
-        this.invalidos = invalidos;
-    }
-
-    public int getErros() {
-        return erros;
-    }
-
-    public void setErros(int erros) {
-        this.erros = erros;
-    }
-
-    @Override
-    public String toString() {
-        return "Total: " + total + "\n"
-                + "Validos:" + (total - invalidos - erros) + ";" + ((float) (total - invalidos - erros) / (float) total) * 100 + "%\n"
-                + "Invalidos: " + invalidos + ";" + ((float) invalidos / (float) total) * 100 + "%\n"
-                + "Erros: " + erros + ";" + ((float) erros / (float) total) * 100 + "%";
+    public static void main(String[] args) {
+        primeiraValidacao();
     }
 
 }
--- InvioScript/src/main/java/bpmlab/invioscript/Main.java
+++ InvioScript/src/main/java/bpmlab/invioscript/Main.java
@@ -5,12 +5,6 @@
  */
 package bpmlab.invioscript;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
-import javax.swing.JFrame;
-import javax.swing.JProgressBar;
-
 /**
  *
  * @author Dedo
@@ -18,30 +12,6 @@ import javax.swing.JProgressBar;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            ConstruirQualis qualis = new ConstruirQualis(true, true);
-            List<Qualis> lista = qualis.contruir();
-            qualis.logEstatistica();
-            Persistencia persistencia = new Persistencia();
-
-//            JFrame frame = new JFrame("Qualis");
-//            frame.setSize(600, 100);
-//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//            frame.setLocationRelativeTo(null);
-//            frame.setVisible(true);
-//            JProgressBar progressBar = new JProgressBar(0, lista.size());
-//            frame.getContentPane().add(progressBar);
-//            
-//            for (int i = 0; i < lista.size(); i++) {
-//                persistencia.salvar(lista.get(i));
-//                progressBar.setValue(i);
-//                progressBar.repaint();
-//            }
-//            persistencia.close();
-        } catch (IOException ex) {
-            System.out.println("ERRO DE LEITURA");
-        } catch (SQLException e) {
-            System.out.println("ERRO DE PERSISTENCIA");
-        }
+       
     }
 }
