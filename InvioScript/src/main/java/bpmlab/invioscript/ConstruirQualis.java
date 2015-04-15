2ae5cc38c65d56896512f32198765d2b7a77dccd
Mikael Lima <mkmikael07@gmail.com>
2015-04-15 00:53:54 -0300

    Update ConstruirQualis.java
    Melhoras

--- InvioScript/src/main/java/bpmlab/invioscript/ConstruirQualis.java
+++ InvioScript/src/main/java/bpmlab/invioscript/ConstruirQualis.java
@@ -9,7 +9,11 @@ import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -17,9 +21,9 @@ import java.util.List;
  */
 public class ConstruirQualis {
 
-    public static List<String> primeiraValidacao() {
+    private static List<String> primeiraValidacao() {
         try {
-            PdfReader pdfReader = new PdfReader("/home/bpmlab/NetBeansProjects/InvioScript/src/main/java/bpmlab/invioscript/Consulta_Webqualis.pdf");
+            PdfReader pdfReader = new PdfReader("C:\\Users\\Dedo\\Documents\\NetBeansProjects\\InvioScript\\src\\main\\java\\bpmlab\\invioscript\\Consulta_Webqualis.pdf");
             String[] linha;
             String novaLinha = null;
             List<String> qualis = new ArrayList<>();
@@ -134,7 +138,7 @@ public class ConstruirQualis {
                 }
             }
             for (String q : qualis) {
-                System.out.println(q);
+//                System.out.println(q);
             }
             System.out.println("TOTAL: " + total);
             System.out.println("VALIDOS: " + qualis.size() + ";" + ((float) qualis.size() * 100 / total) + "%");
@@ -146,8 +150,90 @@ public class ConstruirQualis {
         }
     }
 
+    /**
+      *
+      * Este metodo retorna uma mapa com os seguintes elementos:
+      * <ul>
+      *     <li>qualis : List Qualis</li>
+      *     <li>areas : Set String</li>
+      * </ul>
+      * 
+      * @return Map<String, Object>
+      *
+      **/
+    public static Map<String, Object> construir() {
+        List<String> linhas = primeiraValidacao();
+        List<Qualis> qualis = new ArrayList<>();
+        Set<String> areas = new HashSet<>();
+        for (String linha : linhas) {
+            int indexFinal = linha.indexOf("Atualizado");
+            int indexRevista;
+            String area;
+            String estrato;
+            if (linha.contains(" A1 ")) {
+                estrato = "A1";
+                indexRevista = linha.indexOf(" A1 ");
+                area = linha.substring(linha.indexOf(" A1 ") + 4, indexFinal);
+            } else if (linha.contains(" A2 ")) {
+                estrato = "A2";
+                indexRevista = linha.indexOf(" A2 ");
+                area = linha.substring(linha.indexOf(" A2 ") + 4, indexFinal);
+            } else if (linha.contains(" B1 ")) {
+                estrato = "B1";
+                indexRevista = linha.indexOf(" B1 ");
+                area = linha.substring(linha.indexOf(" B1 ") + 4, indexFinal);
+            } else if (linha.contains(" B2 ")) {
+                estrato = "B2";
+                indexRevista = linha.indexOf(" B2 ");
+                area = linha.substring(linha.indexOf(" B2 ") + 4, indexFinal);
+            } else if (linha.contains(" B3 ")) {
+                estrato = "B3";
+                indexRevista = linha.indexOf(" B3 ");
+                area = linha.substring(linha.indexOf(" B3 ") + 4, indexFinal);
+            } else if (linha.contains(" B4 ")) {
+                estrato = "B4";
+                indexRevista = linha.indexOf(" B4 ");
+                area = linha.substring(linha.indexOf(" B4 ") + 4, indexFinal);
+            } else if (linha.contains(" B5 ")) {
+                estrato = "B5";
+                indexRevista = linha.indexOf(" B5 ");
+                area = linha.substring(linha.indexOf(" B5 ") + 4, indexFinal);
+            } else if (linha.contains(" C ")) {
+                estrato = "C";
+                indexRevista = linha.indexOf(" C ");
+                area = linha.substring(linha.indexOf(" C ") + 3, indexFinal);
+            } else {
+                estrato = "";
+                indexRevista = -1;
+                area = "NADA";
+            }
+            try {
+                if (linha.contains("0101-0794 Revista C & I. Controle & Instrumentação C ENGENHARIAS IV Atualizado")) {
+                    qualis.add(new Qualis("0101-0794", "Revista C & I. Controle & Instrumentação",
+                            "C", "ENGENHARIAS IV", "Atualizado"));
+                    areas.add("ENGENHARIAS IV");
+                } else {
+                    String issn = linha.substring(0, 9);
+                    String titulo = linha.substring(10, indexRevista);
+                    if (titulo.contains("'")) {
+                        titulo = titulo.replace("'", "''");
+                    }
+                    Qualis q = new Qualis(issn.trim(), titulo.trim(), estrato, area.trim(), "Atualizado");
+                    qualis.add(q);
+                    areas.add(area.trim());
+                }
+            } catch (Exception e) {
+            }
+        }
+        areas.remove("");
+        Map<String, Object> params = new HashMap<>();
+        params.put("areas", areas);
+        params.put("qualis", qualis);
+        return params;
+    }
+
     public static void main(String[] args) {
-        primeiraValidacao();
+        construir();
     }
 
 }
